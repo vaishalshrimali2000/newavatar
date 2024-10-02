3c5cb321d00c7c6419c6f00cac48cab5472b40aa
vaishalshrimali2000 <vaishalshrimali.work@gmail.com>
2024-10-02 17:13:03 +0530

    test commit

--- src/components/Page/Pokemon/Addleadsorce/Addleadsorce.tsx
+++ src/components/Page/Pokemon/Addleadsorce/Addleadsorce.tsx
@@ -37,6 +37,7 @@ const ItemsTable = () => {
   const [showDeleteErrorModal, setShowDeleteErrorModal] = useState(false);
   const [deleteErrorMessage, setDeleteErrorMessage] = useState('');
   const [showSuccessModal, setShowSuccessModal] = useState(false);
+
   const apiUrl = 'http://192.168.168.133:90/mst/getantique'; // Your API URL
 
   // Fetch data when component mounts
