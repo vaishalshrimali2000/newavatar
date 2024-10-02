3f12b6b163639f4a9a88ddb1849c02937c92fb1d
vaishalshrimali2000 <your-email@example.com>
2024-10-02 11:19:33 +0530

    aa

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35187e5cee3f639b"; }
+/******/ 		__webpack_require__.h = function() { return "933b9e950af09a9f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ .next/static/webpack/app/(dashboard)/frontform/listusertype/page.30016b99feff0251.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/(dashboard)/frontform/listusertype/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22D%3A%5C%5Cnewavatar_project_new%5C%5Cnode_modules%5C%5Creact-bootstrap%5C%5Cesm%5C%5CCard.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!":
+/*!**************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22D%3A%5C%5Cnewavatar_project_new%5C%5Cnode_modules%5C%5Creact-bootstrap%5C%5Cesm%5C%5CCard.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false! ***!
+  \**************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./node_modules/react-bootstrap/esm/Card.js */ \"(app-pages-browser)/./node_modules/react-bootstrap/esm/Card.js\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIyRCUzQSU1QyU1Q25ld2F2YXRhcl9wcm9qZWN0X25ldyU1QyU1Q25vZGVfbW9kdWxlcyU1QyU1Q3JlYWN0LWJvb3RzdHJhcCU1QyU1Q2VzbSU1QyU1Q0NhcmQuanMlMjIlMkMlMjJpZHMlMjIlM0ElNUIlMjJkZWZhdWx0JTIyJTVEJTdEJnNlcnZlcj1mYWxzZSEiLCJtYXBwaW5ncyI6IkFBQUEsOE1BQXdJIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/MGZiMiJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiwgd2VicGFja0V4cG9ydHM6IFtcImRlZmF1bHRcIl0gKi8gXCJEOlxcXFxuZXdhdmF0YXJfcHJvamVjdF9uZXdcXFxcbm9kZV9tb2R1bGVzXFxcXHJlYWN0LWJvb3RzdHJhcFxcXFxlc21cXFxcQ2FyZC5qc1wiKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22D%3A%5C%5Cnewavatar_project_new%5C%5Cnode_modules%5C%5Creact-bootstrap%5C%5Cesm%5C%5CCard.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!\n"));
+
+/***/ })
+
+});
